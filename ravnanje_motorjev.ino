1fd88a3edacc314f755a150c2555b540a56f9d41
you_found_SaB <166478981+SaB11F@users.noreply.github.com>
2025-04-02 13:25:06 +0200

    motorcki vsi trije

---
+++ ravnanje_motorjev.ino
@@ -0,0 +1,29 @@
+/* Koda za en motor, ki se vrti v eno smer */
+//poravnanje motorja
+
+const int sig1 = 7;
+const int sig2 = 8;
+
+void setup() {
+    pinMode(sig1, OUTPUT);
+    pinMode(sig2, OUTPUT);
+    digitalWrite(sig1, LOW);
+    digitalWrite(sig2, LOW);
+}
+
+void loop() {
+    motor_CW();
+    delay(1000);
+    stopMotor();
+    delay(1000);
+}
+
+void stopMotor() {
+    digitalWrite(sig1, LOW);
+    digitalWrite(sig2, LOW);
+}
+
+void motor_CW() {
+    digitalWrite(sig1, LOW);
+    digitalWrite(sig2, HIGH);
+}
